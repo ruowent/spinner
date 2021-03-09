1d2f0d02ee24eef2cc805676c97d8f7ec54c2db6
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-09 15:38:35 -0500

    added method2 to spinner2 by using setInterval

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,35 @@
-const arr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
-
-for (let i = 100; i < 1800; i+= 200) {
+// const arr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+// let a = 0;
+//console.log(typeof arr[0]);
+// Method 1// using a for loop//
+// for (let i = 100; i < 1800; i+= 200) {
   
-  setTimeout(() => {
+
+//   setTimeout(() => {
+//     if (a === 4) {
+//       a = 0;
+//     }
+
+//     process.stdout.write(arr[a]);
+//     a+=1;
+//   }, i);
+
+// };
+
+// method 2 // use setInterval()
+const arr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+let a = 0;
+
+let interval = setInterval(() => {
+    if (a === 4) {
+      a = 0;
+    }
+
     process.stdout.write(arr[a]);
-  }, i);
-  a+=1;
-};
+    a+=1;
+  }, 100);
+
+setTimeout(() => clearInterval(interval), 20000);
+
+
 
