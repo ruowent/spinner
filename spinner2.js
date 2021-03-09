716b6fd9e86f9c194b1d067d793224c550540bea
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-09 14:44:58 -0500

    refactored spinner1 into spinner2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const arr = ['\r|   ', '\r/   ', '\r-   ', '\r\\   ', '\r|   '];
+
+for (let i = 100; i < 1800; i+= 200) {
+  
+  setTimeout(() => {
+    process.stdout.write(arr[a]);
+  }, i);
+  a+=1;
+};
+
